b942eaf42081cfc140fd6af95899527f27f75500
deepak <deepakjack273001@gmail.com>
2024-07-20 11:43:06 +0530

    initial change

---
+++ js-2-keyword/app.js
@@ -0,0 +1,13 @@
+var a=20;
+var a=30;//variable decalre again and again
+a=40;//value can overwrite 
+a=50;
+console.log(a);
+let b=20;
+//let b=30;error not variable declare again and again
+b=30;//value can assign
+console.log(b);
+const name="jony";
+//const name="stark";variable not declare again again error
+//name="liver"; value not overwrite
+console.log(name);
