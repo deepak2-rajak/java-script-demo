3712624d7dbe77da306d8a5729284adac9aa922f
deepak <deepakjack273001@gmail.com>
2024-07-23 03:37:26 +0530

    new file

---
+++ js-3-data-type/app.js
@@ -0,0 +1,38 @@
+let a=10;
+console.log(a);
+console.log(typeof a);
+let x="hello";
+console.log(x);
+console.log(typeof x);
+let y=true;
+console.log(y);
+console.log(typeof y);
+let b=null;
+console.log(b);
+console.log(typeof b);
+let c=undefined;
+console.log(c);
+console.log(typeof c);
+console.log("non primitive data type");
+
+let student={
+    name:"xyz",
+    class:"12th",
+    gender:"male",
+    age:18,
+    city:"atoz",
+};
+console.log(student);
+console.log(typeof student);
+let arr=[1,2,3,5];
+console.log(arr);
+console.log(typeof arr);
+function hello(){
+    console.log("hi evryone");
+}
+hello();
+console.log(typeof hello);
+
+
+
+
